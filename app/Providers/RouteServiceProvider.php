796207eb1719d005a4ab245d084828d62d7aaa4d
Vento <vento@pfadi-nuenenen.ch>
2021-09-25 17:48:50 +0200

    Change default route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/overwatch';
 
     /**
      * The controller namespace for the application.
